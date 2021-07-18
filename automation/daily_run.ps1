99a2cc6ecbbb8d3eff556324375c9ddf3c42e337
chendi <chenditc@umich.edu>
2021-07-18 13:38:40 +0800

    Add error handling

--- automation/daily_run.ps1
+++ automation/daily_run.ps1
@@ -2,3 +2,4 @@ C:\vnstudio\Scripts\papermill.exe C:\Users\chenditc\Desktop\dev\strategies\cffex
 C:\vnstudio\Scripts\papermill.exe C:\Users\chenditc\Desktop\dev\strategies\cffex_back_testing.ipynb C:\Users\chenditc\Desktop\dev\daily_result\daily_if_backtesting.ipynb -p strategy_name IF --cwd C:\Users\chenditc\Desktop\dev\strategies
 jupyter nbconvert --to html C:\Users\chenditc\Desktop\dev\daily_result\daily_if_backtesting.ipynb
 jupyter nbconvert --to html C:\Users\chenditc\Desktop\dev\daily_result\daily_ic_backtesting.ipynb
+cp  -Recurse -Force C:\Users\chenditc\Desktop\dev\daily_result\ E:\
--- automation/hk_daily_run.ps1
+++ automation/hk_daily_run.ps1
@@ -1,2 +1,3 @@
 C:\vnstudio\Scripts\papermill.exe C:\Users\chenditc\Desktop\dev\strategies\hk_ipo_strategy.ipynb C:\Users\chenditc\Desktop\dev\daily_result\hk_ipo.ipynb --cwd C:\Users\chenditc\Desktop\dev\strategies
 jupyter nbconvert --to html C:\Users\chenditc\Desktop\dev\daily_result\hk_ipo.ipynb
+cp  -Recurse -Force C:\Users\chenditc\Desktop\dev\daily_result\ E:\
