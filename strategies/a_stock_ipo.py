ad1c3d2984694999b16eaf657afea15629d9a1c3
Di <chenditc@umich.edu>
2021-07-23 09:53:43 +0800

    Backup A stock ipo
    备份A股打新脚本，基于讯投的君弘君智做的

---
+++ strategies/a_stock_ipo.py
@@ -0,0 +1,143 @@
+#encoding:gbk
+'''
+自动打新并且开板后卖出
+'''
+import requests
+import datetime
+
+class MyStrategyObject():
+	ipo_complete_map = {}
+	new_stock_tracking_complete_map = {}
+	ipo_bond_complete_map = {}
+
+def send_server_chan(title, text):
+    print(title, text)
+    data = {
+        "title" : title,
+        "desp" : text,
+    }
+    url = f"https://sctapi.ftqq.com/xxx.send"
+    response = requests.post(url, data=data)
+    print(response.json()["message"])
+    
+def get_applied_code_set(ContextInfo):
+	applied_code_set = set()
+	order_list = get_trade_detail_data(ContextInfo.accID, "STOCK", "ORDER")
+	for order in order_list:
+		if order.m_nOrderStatus == 57: #  废单
+			continue
+		elif order.m_nOrderStatus == 50:
+			stock_code = f"{order.m_strInstrumentID}.{order.m_strExchangeID}"
+			applied_code_set.add(stock_code)
+		else:
+			print(f"Unknown status code: {order.m_strCancelInfo}")
+	return applied_code_set
+	
+def get_blacklist_fail_code(ContextInfo, threshold=6):
+	failed_reason_map = {}
+	order_list = get_trade_detail_data(ContextInfo.accID, "STOCK", "ORDER")
+	for order in order_list:
+		if order.m_nOrderStatus == 57: #  废单
+			stock_code = f"{order.m_strInstrumentID}.{order.m_strExchangeID}"
+			print(stock_code, order.m_strCancelInfo)
+			reason_list = failed_reason_map.get(stock_code, [])
+			reason_list.append(order.m_strCancelInfo)
+			failed_reason_map[stock_code] = reason_list
+
+	blacklist_code = {}
+	for code, reason_list in failed_reason_map.items():
+		if len(reason_list) > threshold:
+			blacklist_code[code] = set(reason_list)
+	return blacklist_code
+	
+def init(ContextInfo):
+	print("Init strategy")
+	ContextInfo.accID = '102666'
+	ContextInfo.strategy_obj = MyStrategyObject()
+	
+def handlebar(ContextInfo):
+	# Only last bar can trade
+	if not ContextInfo.is_last_bar():
+		return 
+	
+	# First bar is not stable
+	if 	ContextInfo.time_tick_size <= 1:
+		return
+		
+	# Run once for each bar, when new bar is generated
+	if not ContextInfo.is_new_bar():
+		return
+		
+	current_time_str = datetime.datetime.now().strftime("%H%M")
+	if current_time_str < "0930" or current_time_str > "1500":
+		print("不在交易时间")
+		return
+		
+	if current_time_str > "1130" and current_time_str < "1300":
+		print("不在交易时间")
+		return
+		
+	today_date = datetime.datetime.today().date().strftime("%Y-%m-%d")
+
+	
+	if today_date not in ContextInfo.strategy_obj.ipo_complete_map:
+		# 获取当日可打新股
+		print("开始打新股")
+		ipo_stock = get_ipo_data("STOCK")
+		ipo_bond = get_ipo_data("BOND")
+		ipo_stock.update(ipo_bond)
+		print("今日新股：", ipo_stock)
+		blacklist_code = get_blacklist_fail_code(ContextInfo)
+		applied_code_set = get_applied_code_set(ContextInfo)
+		print("applied code:", applied_code_set)
+		print("black list code:", blacklist_code)
+		
+		ipo_stock_to_apply = [ stock_code for stock_code in ipo_stock.keys() if 
+			(stock_code not in applied_code_set and stock_code not in blacklist_code)]
+		
+		if len(ipo_stock_to_apply) == 0:
+			text = f"{today_date}所有新股完成申购完成，已申购成功： {applied_code_set}，申购失败： {blacklist_code}"
+			send_server_chan("A股新股申购完成", text)
+			ContextInfo.strategy_obj.ipo_complete_map[today_date] = text
+		
+		# 针对每只新股下单
+		for stock_code in ipo_stock_to_apply:
+			stock_info = ipo_stock[stock_code]
+			print(f"Place ipo order for {stock_code}")
+			#order_shares(stock_code, stock_info["maxPurchaseNum"], ContextInfo, ContextInfo.accID)
+			passorder(23,1101,ContextInfo.accID,stock_code,5,-1,stock_info["maxPurchaseNum"],1,ContextInfo)
+	
+	if today_date not in ContextInfo.strategy_obj.new_stock_tracking_complete_map:
+		print("开始跟踪新股走势")
+		# 查看当前持仓情况，获取次新股列表
+		two_month_ago = (datetime.datetime.today() - datetime.timedelta(days=60)).strftime("%Y%m%d")
+		position_list = get_trade_detail_data(ContextInfo.accID, "STOCK", "POSITION")
+		new_pos_map = {}
+		for position in position_list:
+			stock_code = f"{position.m_strInstrumentID}.{position.m_strExchangeID}"
+			open_date = ContextInfo.get_open_date(stock_code)
+			up_stop_price = ContextInfo.get_instrumentdetail(stock_code)["UpStopPrice"]
+			last_price = ContextInfo.get_market_data(["close"], stock_code = [stock_code], skip_paused = True, period = '1m', dividend_type = 'none', count = -1)
+			if str(open_date) > two_month_ago:
+				new_pos_map[stock_code] = {
+					"up_stop_price" : up_stop_price,
+					"name" : position.m_strInstrumentName,
+					"open_date": str(open_date),
+					"last_price" : last_price
+				}
+			print(stock_code, position.m_strInstrumentName, open_date)
+		print("当前新股持仓: ", new_pos_map)
+		
+		if len(new_pos_map) == 0:
+			print("新股跟踪已完成")
+			ContextInfo.strategy_obj.new_stock_tracking_complete_map[today_date] = "No new stock"
+	
+		# 查看对应股票的最新价是否为涨停价
+		for stock_code, pos_info in new_pos_map.items():
+			if pos_info["last_price"] < pos_info["up_stop_price"] * 0.99:
+				text = f"新股开板，应当卖出{stock_code}"
+				send_server_chan("A股新股卖出", text)
+		
+		# 卖出非涨停且不在上涨趋势的股票，上涨趋势定义为30分钟均线高于3小时均线。
+	
+	print("======================================================")
